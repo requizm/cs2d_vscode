461c7510bfd1d9fd8d3b46fbac38fe94058416bd
Menderes <mehmet41650@gmail.com>
2020-03-08 21:03:27 +0300

    line çizimi daha efektif
    -Önceden neden yapmadım bilmiyorum ama artık glLineWidth kullanıyorum.

--- src/Renderers/SquareRenderer.cpp
+++ src/Renderers/SquareRenderer.cpp
@@ -21,7 +21,7 @@ SquareRenderer::SquareRenderer(bool init)
 
 SquareRenderer::~SquareRenderer() = default;
 
-void SquareRenderer::ui_RenderFilledSquare(glm::vec2 position, glm::vec2 size, glm::vec3 color, glm::vec3 borderColor, float borderSize, GLfloat transperancy, GLfloat rotate)
+void SquareRenderer::ui_RenderFilledSquare(glm::vec2 position, glm::vec2 size, glm::vec3 color, bool outline, glm::vec3 borderColor, float borderSize, GLfloat transperancy, GLfloat rotate)
 {
 	// Prepare transformations
 	this->squareShader_ui.Use();
@@ -40,7 +40,8 @@ void SquareRenderer::ui_RenderFilledSquare(glm::vec2 position, glm::vec2 size, g
 	glBindVertexArray(0);
 	this->squareShader_ui.UnUse();
 
-	ui_RenderEmptySquareWithLine(position, size, borderColor, borderSize, transperancy, rotate);
+	if(outline)
+		ui_RenderEmptySquareWithLine(position, size, borderColor, borderSize  / 2, transperancy, rotate);
 }
 
 void SquareRenderer::ui_RenderFilledSquare(glm::vec2 position, glm::vec2 size, glm::vec3 color, GLfloat transperancy, GLfloat rotate)
@@ -83,72 +84,166 @@ void SquareRenderer::world_RenderFilledSquare(glm::vec2 position, glm::vec2 size
 
 void SquareRenderer::world_RenderEmptySquareWithLine(glm::vec2 position, glm::vec2 size, glm::vec3 color, float lineSize, GLfloat transperancy, GLfloat rotate)
 {
-	glm::vec2 pos, siz;
-
+	//glm::vec2 pos, siz;
+	glm::vec2 startPos, endPos;
 	//sol kenar
-	pos = glm::vec2(position.x - lineSize, position.y);
+	/*pos = glm::vec2(position.x - lineSize, position.y);
 	siz = glm::vec2(lineSize * 2, size.y);
-	this->world_RenderFilledSquare(pos, siz, color, transperancy, rotate);
+	this->world_RenderFilledSquare(pos, siz, color, transperancy, rotate);*/
+	startPos = position;
+	endPos = glm::vec2(position.x, position.y + size.y);
+	this->world_RenderLine(startPos, endPos, color, lineSize, transperancy);
 
 	//ust kenar
-	pos = glm::vec2(position.x, position.y - lineSize);
+	/*pos = glm::vec2(position.x, position.y - lineSize);
 	siz = glm::vec2(size.x, lineSize * 2);
-	this->world_RenderFilledSquare(pos, siz, color, transperancy, rotate);
+	this->world_RenderFilledSquare(pos, siz, color, transperancy, rotate);*/
+	startPos = position;
+	endPos = glm::vec2(position.x + size.x, position.y);
+	this->world_RenderLine(startPos, endPos, color, lineSize, transperancy);
 
 	//sag kenar
-	pos = glm::vec2(position.x + size.x - lineSize, position.y);
+	/*pos = glm::vec2(position.x + size.x - lineSize, position.y);
 	siz = glm::vec2(lineSize * 2, size.y);
-	this->world_RenderFilledSquare(pos, siz, color, transperancy, rotate);
+	this->world_RenderFilledSquare(pos, siz, color, transperancy, rotate);*/
+	startPos = glm::vec2(position.x + size.x, position.y);
+	endPos = glm::vec2(position.x + size.x, position.y + size.y);
+	this->world_RenderLine(startPos, endPos, color, lineSize, transperancy);
 
 	//alt kenar
-	pos = glm::vec2(position.x, position.y + size.y - lineSize);
+	/*pos = glm::vec2(position.x, position.y + size.y - lineSize);
 	siz = glm::vec2(size.x, lineSize * 2);
-	this->world_RenderFilledSquare(pos, siz, color, transperancy, rotate);
+	this->world_RenderFilledSquare(pos, siz, color, transperancy, rotate);*/
+	startPos = glm::vec2(position.x, position.y + size.y);
+	endPos = glm::vec2(position.x + size.x, position.y + size.y);
+	this->world_RenderLine(startPos, endPos, color, lineSize, transperancy);
 }
 
 void SquareRenderer::ui_RenderEmptySquareWithLine(glm::vec2 position, glm::vec2 size, glm::vec3 color, float lineSize, GLfloat transperancy, GLfloat rotate)
 {
-	glm::vec2 pos, siz;
-
+	//glm::vec2 pos, siz;
+	glm::vec2 startPos, endPos;
 	//sol kenar
-	pos = glm::vec2(position.x - lineSize / 2, position.y);
-	siz = glm::vec2(lineSize / 2 * 2, size.y);
-	this->ui_RenderFilledSquare(pos, siz, color, transperancy, rotate);
+	/*pos = glm::vec2(position.x - lineSize, position.y);
+	siz = glm::vec2(lineSize * 2, size.y);
+	this->world_RenderFilledSquare(pos, siz, color, transperancy, rotate);*/
+	startPos = position;
+	endPos = glm::vec2(position.x, position.y + size.y);
+	this->ui_RenderLine(startPos, endPos, color, lineSize, transperancy);
 
 	//ust kenar
-	pos = glm::vec2(position.x, position.y - lineSize / 2);
-	siz = glm::vec2(size.x, lineSize / 2 * 2);
-	this->ui_RenderFilledSquare(pos, siz, color, transperancy, rotate);
+	/*pos = glm::vec2(position.x, position.y - lineSize);
+	siz = glm::vec2(size.x, lineSize * 2);
+	this->world_RenderFilledSquare(pos, siz, color, transperancy, rotate);*/
+	startPos = position;
+	endPos = glm::vec2(position.x + size.x, position.y);
+	this->ui_RenderLine(startPos, endPos, color, lineSize, transperancy);
 
 	//sag kenar
-	pos = glm::vec2(position.x + size.x - lineSize / 2, position.y);
-	siz = glm::vec2(lineSize / 2 * 2, size.y);
-	this->ui_RenderFilledSquare(pos, siz, color, transperancy, rotate);
+	/*pos = glm::vec2(position.x + size.x - lineSize, position.y);
+	siz = glm::vec2(lineSize * 2, size.y);
+	this->world_RenderFilledSquare(pos, siz, color, transperancy, rotate);*/
+	startPos = glm::vec2(position.x + size.x, position.y);
+	endPos = glm::vec2(position.x + size.x, position.y + size.y);
+	this->ui_RenderLine(startPos, endPos, color, lineSize, transperancy);
 
 	//alt kenar
-	pos = glm::vec2(position.x, position.y + size.y - lineSize / 2);
-	siz = glm::vec2(size.x, lineSize / 2 * 2);
-	this->ui_RenderFilledSquare(pos, siz, color, transperancy, rotate);
+	/*pos = glm::vec2(position.x, position.y + size.y - lineSize);
+	siz = glm::vec2(size.x, lineSize * 2);
+	this->world_RenderFilledSquare(pos, siz, color, transperancy, rotate);*/
+	startPos = glm::vec2(position.x, position.y + size.y);
+	endPos = glm::vec2(position.x + size.x, position.y + size.y);
+	this->ui_RenderLine(startPos, endPos, color, lineSize, transperancy);
 }
 
-void SquareRenderer::RenderLine(glm::vec2 position, glm::vec2 size, glm::vec3 color, GLfloat transperancy, GLfloat rotate)
+void SquareRenderer::ui_RenderLine(glm::vec2 startPos, glm::vec2 endPos, glm::vec3 color, float lineSize, GLfloat transperancy)
 {
+	glm::vec2 size;
+	int rotate = 0;
+	glm::vec2 dif = endPos - startPos;
+	if (abs(dif.y) >= abs(dif.x))
+	{
+		size = glm::vec2(dif.y, lineSize);
+		dif = glm::normalize(dif);
+		rotate = glm::degrees(atan2(dif.x, dif.y));
+		if (dif.y >= 0)
+			rotate += 90;
+		else
+			rotate -= 90;
+	}
+	else if (abs(dif.x) > abs(dif.y))
+	{
+		size = glm::vec2(dif.x, lineSize);
+		dif = glm::normalize(dif);
+		rotate = glm::degrees(atan2(dif.x, dif.y));
+		if (dif.x >= 0)
+			rotate -= 90;
+		else
+			rotate += 90;
+	}
+
 	// Prepare transformations
 	this->squareShader_ui.Use();
 	glm::mat4 model = glm::mat4(1.0);
-	model = glm::translate(model, glm::vec3(position, 0.0f)); // First translate (transformations are: scale happens first, then rotation and then finall translation happens; reversed order)
-	//model = glm::translate(model, glm::vec3(position.x, position.y, 0.0f));
-	model = glm::translate(model, glm::vec3(0.5f * size.x, 0.5f * size.y, 0.0f));   // Move origin of rotation to center of quad
-	model = glm::rotate(model, glm::radians(rotate), glm::vec3(0.0f, 0.0f, 1.0f));  // Then rotate
-	model = glm::translate(model, glm::vec3(-0.5f * size.x, -0.5f * size.y, 0.0f)); // Move origin back
-	model = glm::scale(model, glm::vec3(size, 1.0f));								// Last scale
+	model = glm::translate(model, glm::vec3(startPos, 0.0f)); // First translate (transformations are: scale happens first, then rotation and then finall translation happens; reversed order)
+	//model = glm::translate(model, glm::vec3(0.5f * size.x, 0.5f * size.y, 0.0f));					   // Move origin of rotation to center of quad
+	model = glm::rotate(model, glm::radians(static_cast<float>(rotate)), glm::vec3(0.0f, 0.0f, 1.0f)); // Then rotate
+	//model = glm::translate(model, glm::vec3(-0.5f * size.x, -0.5f * size.y, 0.0f));					   // Move origin back
+	model = glm::scale(model, glm::vec3(size, 1.0f)); // Last scale
 	this->squareShader_ui.SetMatrix4("model", model);
 	this->squareShader_ui.SetVector3f("colorUniform", color);
 	this->squareShader_ui.SetFloat("trans", transperancy);
+	glLineWidth(lineSize);
 	glBindVertexArray(outline.VAO);
-	glDrawArrays(GL_LINES, 0, 4);
+	glDrawArrays(GL_LINE_LOOP, 0, 4);
 	glBindVertexArray(0);
+	glLineWidth(1.0F);
 	this->squareShader_ui.UnUse();
+}
+
+void SquareRenderer::world_RenderLine(glm::vec2 startPos, glm::vec2 endPos, glm::vec3 color, float lineSize, GLfloat transperancy)
+{
+	glm::vec2 size;
+	int rotate = 0;
+	glm::vec2 dif = endPos - startPos;
+	if (abs(dif.y) >= abs(dif.x))
+	{
+		size = glm::vec2(dif.y, lineSize);
+		dif = glm::normalize(dif);
+		rotate = glm::degrees(atan2(dif.x, dif.y));
+		if (dif.y >= 0)
+			rotate += 90;
+		else
+			rotate -= 90;
+	}
+	else if (abs(dif.x) > abs(dif.y))
+	{
+		size = glm::vec2(dif.x, lineSize);
+		dif = glm::normalize(dif);
+		rotate = glm::degrees(atan2(dif.x, dif.y));
+		if (dif.x >= 0)
+			rotate -= 90;
+		else
+			rotate += 90;
+	}
+
+	// Prepare transformations
+	this->squareShader_world.Use();
+	glm::mat4 model = glm::mat4(1.0);
+	model = glm::translate(model, glm::vec3(startPos, 0.0f)); // First translate (transformations are: scale happens first, then rotation and then finall translation happens; reversed order)
+	//model = glm::translate(model, glm::vec3(0.5f * size.x, 0.5f * size.y, 0.0f));					   // Move origin of rotation to center of quad
+	model = glm::rotate(model, glm::radians(static_cast<float>(rotate)), glm::vec3(0.0f, 0.0f, 1.0f)); // Then rotate
+	//model = glm::translate(model, glm::vec3(-0.5f * size.x, -0.5f * size.y, 0.0f));					   // Move origin back
+	model = glm::scale(model, glm::vec3(size, 1.0f)); // Last scale
+	this->squareShader_world.SetMatrix4("model", model);
+	this->squareShader_world.SetVector3f("colorUniform", color);
+	this->squareShader_world.SetFloat("trans", transperancy);
+	glLineWidth(lineSize);
+	glBindVertexArray(outline.VAO);
+	glDrawArrays(GL_LINE_LOOP, 0, 4);
+	glBindVertexArray(0);
+	glLineWidth(1.0F);
+	this->squareShader_world.UnUse();
 }
 
 void SquareRenderer::ui_RenderEmptySquare(glm::vec2 position, glm::vec2 size, glm::vec3 color, GLfloat transperancy, GLfloat rotate)
--- src/Renderers/SquareRenderer.h
+++ src/Renderers/SquareRenderer.h
@@ -21,13 +21,14 @@ public:
 	Square circle;
 	//Outline title_line = Outline();
 
-	void ui_RenderFilledSquare(glm::vec2 position, glm::vec2 size, glm::vec3 color, glm::vec3 borderColor, float borderSize, GLfloat transperancy = 1.0F, GLfloat rotate = 0.0F);
+	void ui_RenderFilledSquare(glm::vec2 position, glm::vec2 size, glm::vec3 color, bool outline, glm::vec3 borderColor, float borderSize, GLfloat transperancy = 1.0F, GLfloat rotate = 0.0F);
 	void ui_RenderFilledSquare(glm::vec2 position, glm::vec2 size, glm::vec3 color, GLfloat transperancy = 1.0F, GLfloat rotate = 0.0F);
 	void world_RenderFilledSquare(glm::vec2 position, glm::vec2 size, glm::vec3 color, GLfloat transperancy = 1.0F, GLfloat rotate = 0.0F);
 	void world_RenderEmptySquareWithLine(glm::vec2 position, glm::vec2 size, glm::vec3 color, float lineSize, GLfloat transperancy = 1.0F, GLfloat rotate = 0.0F);
 	void ui_RenderEmptySquareWithLine(glm::vec2 position, glm::vec2 size, glm::vec3 color, float lineSize, GLfloat transperancy = 1.0F, GLfloat rotate = 0.0F);
 
-	void RenderLine(glm::vec2 position, glm::vec2 size, glm::vec3 color, GLfloat transperancy = 1.0F, GLfloat rotate = 0.0F);
+	void ui_RenderLine(glm::vec2 startPos, glm::vec2 endPos, glm::vec3 color, float lineSize = 1.0F, GLfloat transperancy = 1.0F);
+	void world_RenderLine(glm::vec2 startPos, glm::vec2 endPos, glm::vec3 color, float lineSize = 1.0F, GLfloat transperancy = 1.0F);
 	void ui_RenderEmptySquare(glm::vec2 position, glm::vec2 size, glm::vec3 color, GLfloat transperancy = 1.0F, GLfloat rotate = 0.0F);
 	void world_RenderEmptySquare(glm::vec2 position, glm::vec2 size, glm::vec3 color, GLfloat transperancy = 1.0F, GLfloat rotate = 0.0F);
 
