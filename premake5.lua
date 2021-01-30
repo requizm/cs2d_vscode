2836e64678576e8447d137c580c1fd0339a406f1
requizm <mehmet416500@hotmail.com>
2021-01-30 18:31:14 +0300

    premake eklendi, glfw3 statik linklendi
    -oncedeki link kodlarima baktim, ilginc bir sekilde *.a dosyalarini elle girmisim. Artik daha efektik oldu. glfw3.dll dosyasina gerek kalmadi. 
    -cmakelist.txt dosyasini hala ayip olmasin diye tutuyorum. isteyen oradan da build eder ancak tavsiyem premake kullanmaniz. 
    -premake veya cmake kullanacaklarin dikkatine, build etmeden once dogru glfw3 lib dosyalarini lib/32 icine koyun.

---
+++ premake5.lua
@@ -0,0 +1,26 @@
+workspace "cs2d_vscode"
+   configurations { "Release" }
+
+project "cs2d_vscode"
+   kind "ConsoleApp"
+   language "C++"
+   cppdialect "C++17"
+   targetdir "bin/%{cfg.buildcfg}"
+
+   files { "src/**.hpp", "src/**.cpp" }
+   removefiles { "src/Common.cpp", "src/Common.hpp" }
+   
+   libdirs { "lib/32"}
+   includedirs { "include/" }
+	
+   links { "glew32", "glfw3", "opengl32", "gdi32", "freetype" }
+
+   filter "configurations:Release"
+      defines { "NDEBUG" }
+	  
+	
+	
+
+	
+	
+	
