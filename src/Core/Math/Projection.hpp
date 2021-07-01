7d10ebda3ceb54a5745ebfdf52602d35e0f68273
requizm <mehmet41650@gmail.com>
2021-07-01 21:09:43 +0300

    error fix

--- src/Core/Math/Projection.hpp
+++ src/Core/Math/Projection.hpp
@@ -269,7 +269,7 @@ class Projection
 
         det = m[0] * inv[0] + m[1] * inv[4] + m[2] * inv[8] + m[3] * inv[12];
 
-        if (det == 0) return false;
+        if (det == 0) return Matrix4<float>(-1.0F);
 
         det = 1.0F / det;
 
